a937d7823044943728349e43499e66b76caa9476
bwilk86 <bwilkie21@hotmail.com>
2017-09-05 02:37:48 +0000

    Working ignition simulator

--- IgnitionSimulator.py
+++ IgnitionSimulator.py
@@ -9,21 +9,41 @@ GPIO.setup(acc_pin, GPIO.OUT)
 
 
 def NormalStartCar():
-	if GPIO.output(acc_pin)== LOW:
-		GPIO.output(acc_pin, 1)
-		time.sleep(15)
-	else:
-	    print("Already Started, Stop Car first")
+    if GPIO.input(acc_pin)== 0:
+        GPIO.output(acc_pin, 1)
+        x=0
+        while x<15:
+            time.sleep(1)
+            print(x)
+            x+=1
+    else:
+        print("Already Started, Stop Car first")
+
+def QuickStart():
+    if GPIO.input(acc_pin)== 0:
+        GPIO.output(acc_pin, 1)
+    else:
+        print("Already Started, Stop Car first")
 
 def NormalStopCar():
-	if GPIO.input(acc_pin)==1:
-		GPIO.output(acc_pin, 0)
-		time.sleep(15)
-	else:
-            print("Car already stopped, start car first")
+    if GPIO.input(acc_pin)==1:
+        GPIO.output(acc_pin, 0)
+        x=0
+        while x<15:
+            time.sleep(1)
+            print(x)
+            x+=1
+    else:
+        print("Car already stopped, start car first")
+def QuickStop():
+    if GPIO.input(acc_pin)==1:
+        GPIO.output(acc_pin, 0)
+    else:
+        print("Car already stopped, start car first")
+
 def InterruptedStartCar():
 	if GPIO.input(acc_pin)==0:
-		GPIO.ouput(acc_pin, 1)
+		GPIO.output(acc_pin, 1)
 		time.sleep(3)
 		GPIO.output(acc_pin, 0)
 	else:
@@ -38,21 +58,29 @@ def StopEngineThenTurnAccessoriesOn():
 		print("Car already stopped, start car first")
 def Shutdown():
     GPIO.cleanup()
-    
+
+blah = True;  
 try:
-    while True:
-	ignition_input = input("Start - Stop - Interrupt - StopAcc")
-	if ignition_input.lower=="start":
-            NormalStartCar()
-            print(GPIO.input(acc_pin))
-	elif ignition_input.lower=="stop":
-            NormalStopCar()
-            print(GPIO.input(acc_pin))
-	elif ignition_input.lower=="interrupt":
-            InterruptedStartCar()
-            print(GPIO.input(acc_pin))
-	elif ignition_input.lower=="stopacc":
-            StopEngineThenTurnAccessoriesOn()
-            print(GPIO.input(acc_pin))
+    while blah:
+            ignition_input = input("Start - Stop - Interrupt - StopAcc - QStart - QStop: \n")
+            if ignition_input.lower()=="start":
+                NormalStartCar()
+                print(GPIO.input(acc_pin))
+            elif ignition_input.lower()=="stop":
+                NormalStopCar()
+                print(GPIO.input(acc_pin))
+            elif ignition_input.lower()=="interrupt":
+                InterruptedStartCar()
+                print(GPIO.input(acc_pin))
+            elif ignition_input.lower()=="stopacc":
+                StopEngineThenTurnAccessoriesOn()
+                print(GPIO.input(acc_pin))
+            elif ignition_input.lower()=="qstart":
+                QuickStart()
+            elif ignition_input.lower()=="qstop":
+                QuickStop()
+            elif ignition_input.lower()=="exit":
+                Shutdown()
+                blah = False
 except KeyboardInterrupt:
                      Shutdown()
